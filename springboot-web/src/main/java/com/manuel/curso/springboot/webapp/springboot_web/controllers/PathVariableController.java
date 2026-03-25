3ff19c56085c7b0e910289f5ff1d012085cabff1
Manuel Gordillo <manuelgordillodeveloper@gmail.com>
2026-03-25 15:40:51 -0500

    @Request

---
+++ springboot-web/src/main/java/com/manuel/curso/springboot/webapp/springboot_web/controllers/PathVariableController.java
@@ -0,0 +1,38 @@
+package com.manuel.curso.springboot.webapp.springboot_web.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.manuel.curso.springboot.webapp.springboot_web.models.DTO.ParamDto;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+
+
+
+@RestController
+@RequestMapping("api/var")
+public class PathVariableController {
+  @GetMapping("/baz/{message}")
+  public ParamDto baz(@PathVariable() String message) {
+    ParamDto param = new ParamDto();
+    param.setMessage(message);
+    return param;
+  }
+    
+  @GetMapping("/mix/{product}/{id}")
+  public Map<String, Object> mixPathVar(@PathVariable String product, @PathVariable Long id) {
+    Map<String, Object> json = new HashMap<>();
+    json.put("product", product);
+    json.put("id", id);
+    
+    return json;
+    }  
+}
+  
+
---
+++ springboot-web/src/main/java/com/manuel/curso/springboot/webapp/springboot_web/models/DTO/ParamDto.java
@@ -0,0 +1,18 @@
+package com.manuel.curso.springboot.webapp.springboot_web.models.DTO;
+
+public class ParamDto {
+  private String message;
+  
+
+  public String getMessage() {
+    return message;
+  }
+
+  public void setMessage(String message) {
+    this.message = message;
+  }
+
+  
+
+  
+}
