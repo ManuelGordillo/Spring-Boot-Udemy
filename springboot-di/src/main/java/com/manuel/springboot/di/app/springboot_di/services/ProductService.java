3efd817302a100e9ef9b6e09e9122919dedae39a
Manuel Gordillo <manuelgordillodeveloper@gmail.com>
2026-03-30 17:21:20 -0500

    Inmtabilidad con implements Clonable

---
+++ springboot-di/src/main/java/com/manuel/springboot/di/app/springboot_di/services/ProductService.java
@@ -0,0 +1,26 @@
+package com.manuel.springboot.di.app.springboot_di.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.manuel.springboot.di.app.springboot_di.models.Product;
+import com.manuel.springboot.di.app.springboot_di.repositories.ProductRepository;
+
+public class ProductService {
+
+  private ProductRepository repository = new ProductRepository();
+
+  public List<Product> findAll() {
+    return repository.findAll().stream().map(p -> {
+      Double priceTax = p.getPrice() * 1.25d;
+      // Product newProd = new Product(p.getId(),p.getName(), priceTax.longValue());
+      Product newProd = (Product) p.clone();
+      newProd.setPrice(priceTax.longValue());
+      return newProd;
+    }).collect(Collectors.toList());
+  }
+
+  public Product findById(Long id) {
+    return repository.findById(id);
+  }
+}
