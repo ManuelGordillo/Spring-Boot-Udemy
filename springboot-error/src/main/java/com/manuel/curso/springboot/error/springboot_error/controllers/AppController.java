05d6a0274149360b222f3e239dacd4a46c2d73e0
Manuel Gordillo <manuelgordillodeveloper@gmail.com>
2026-04-23 17:58:26 -0500

    Creacion de Exceptions Personalizadas

---
+++ springboot-error/src/main/java/com/manuel/curso/springboot/error/springboot_error/controllers/AppController.java
@@ -0,0 +1,44 @@
+package com.manuel.curso.springboot.error.springboot_error.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.manuel.curso.springboot.error.springboot_error.exceptions.UserNotFoundException;
+import com.manuel.curso.springboot.error.springboot_error.models.domain.User;
+import com.manuel.curso.springboot.error.springboot_error.services.UserService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+
+
+@RestController
+@RequestMapping("/app")
+public class AppController {
+
+  @Autowired
+  private UserService service;
+  
+  @GetMapping("")
+  public String index() {
+    // int value = 100 / 0;
+    int value = Integer.parseInt("10");
+    System.out.println(value);
+    return "ok 200";
+  }
+
+  @GetMapping("/show/{id}")
+  public User show(@PathVariable(name = "id") Long id) {
+    User user = service.findById(id);
+    if (user == null) {
+      throw new UserNotFoundException("Error el usuario no existe");
+    }
+    System.out.println(user.getLastname());
+    return user;
+  }
+  
+  
+}
