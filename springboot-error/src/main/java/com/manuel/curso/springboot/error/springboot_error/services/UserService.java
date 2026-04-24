8bf07f63466a89851b34a3cac339c2a47024d994
ManuelGordillo <manuelgordillodeveloper@gmail.com>
2026-04-24 00:32:18 -0500

    Excepciones terminadas - siguiente crear interface e inyectarla

--- springboot-error/src/main/java/com/manuel/curso/springboot/error/springboot_error/services/UserService.java
+++ springboot-error/src/main/java/com/manuel/curso/springboot/error/springboot_error/services/UserService.java
@@ -1,11 +1,12 @@
 package com.manuel.curso.springboot.error.springboot_error.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.manuel.curso.springboot.error.springboot_error.models.domain.User;
 
 public interface UserService {
 
   List<User> findAll();
-  User findById(Long id);
+  Optional<User> findById(Long id);
 }
