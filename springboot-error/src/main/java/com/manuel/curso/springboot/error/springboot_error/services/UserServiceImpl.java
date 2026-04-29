175240bccf72a36231018301b3b480b19713deb5
ManuelGordillo <manuelgordillodeveloper@gmail.com>
2026-04-29 10:26:57 -0500

    seccion de errores

--- springboot-error/src/main/java/com/manuel/curso/springboot/error/springboot_error/services/UserServiceImpl.java
+++ springboot-error/src/main/java/com/manuel/curso/springboot/error/springboot_error/services/UserServiceImpl.java
@@ -1,9 +1,10 @@
 package com.manuel.curso.springboot.error.springboot_error.services;
 
-import java.util.ArrayList;
+// import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.manuel.curso.springboot.error.springboot_error.models.domain.User;
@@ -11,17 +12,10 @@ import com.manuel.curso.springboot.error.springboot_error.models.domain.User;
 @Service
 public class UserServiceImpl implements UserService {
   
-  
+  @Autowired
   private List<User> users;
 
-  public UserServiceImpl() {
-    this.users = new ArrayList<>();
-    users.add(new User(1L,"Pepe","Gonzales"));
-    users.add(new User(2L,"Andres","Mena"));
-    users.add(new User(3L,"Maria","Perez"));
-    users.add(new User(4L,"Josefa","Ramires"));
-    users.add(new User(5L,"Ale","Gutierrez"));
-  }
+
 
   @Override
   public List<User> findAll() {
@@ -30,14 +24,14 @@ public class UserServiceImpl implements UserService {
 
   @Override
   public Optional<User> findById(Long id) {
-    User user = null;
-    for (User u : users) {
-      if (u.getId().equals(id)) {
-        user = u;
-        break;
-      }
-    }
-    return Optional.ofNullable(user);
+    Optional <User> user = users.stream().filter(usr -> usr.getId().equals(id)).findFirst();
+    // for (User u : users) {
+    //   if (u.getId().equals(id)) {
+    //     user = u;
+    //     break;
+    //   }
+    // }
+    return user;
   }
 
 }
