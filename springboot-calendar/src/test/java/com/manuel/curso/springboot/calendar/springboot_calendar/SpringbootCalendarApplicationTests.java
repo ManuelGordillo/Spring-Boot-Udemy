d51e2b40a6f9804eec0fce0aaef944bc27340a28
Manuel Gordillo <manuelgordillodeveloper@gmail.com>
2026-04-29 18:24:51 -0500

    Interceptores creados y personalizados

---
+++ springboot-calendar/src/test/java/com/manuel/curso/springboot/calendar/springboot_calendar/SpringbootCalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manuel.curso.springboot.calendar.springboot_calendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCalendarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
