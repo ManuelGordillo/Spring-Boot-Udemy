c02f1a1076ead819c6fc4f5649360e22148935da
ManuelGordillo <manuelgordillodeveloper@gmail.com>
2026-05-19 17:17:12 -0500

    Iniciando con JPA , AOP no compatible

---
+++ springbootjpa/src/main/java/com/manuel/curso/sprinboot/jpa/springbootjpa/SpringbootjpaApplication.java
@@ -0,0 +1,19 @@
+package com.manuel.curso.sprinboot.jpa.springbootjpa;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootjpaApplication implements CommandLineRunner{
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootjpaApplication.class, args);
+	}
+
+  @Override
+  public void run(String... args) throws Exception {
+    
+  }
+
+}
